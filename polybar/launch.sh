d3278bf8d2475e3a9454361b8d535dc907a8b7e0
martin.grgv <martin.geor04@gmail.com>
2025-03-05 23:06:18 +0200

    Polybar all monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,7 +3,11 @@
 # Terminating running bars
 polybar-msg cmd quit
 
-# Launch polybar
-polybar bar 2>&1 | tee a /tmp/polybar1.log & disown
-
-#echo "Bar launched."
+# Launch polybar on all monitors
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload bar &
+	done
+else
+	polybar --reload bar &
+fi
