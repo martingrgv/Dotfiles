c546ca0f226bb38ac949bd0660efb7a61d71b868
martin.grgv <martin.geor04@gmail.com>
2025-08-06 03:10:47 +0300

    roslyn fix + razer working

--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -8,7 +8,20 @@ return {
 		},
 	},
 	config = function()
-		local cmd = {}
+		local mason_registry = require("mason-registry")
+
+		local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
+		local cmd = {
+			"roslyn",
+			"--stdio",
+			"--logLevel=Information",
+			"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+			"--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+			"--razorDesignTimePath="
+				.. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+			"--extension",
+			vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
+		}
 
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
@@ -20,17 +33,6 @@ return {
 				["csharp|inlay_hints"] = {
 					csharp_enable_inlay_hints_for_implicit_object_creation = true,
 					csharp_enable_inlay_hints_for_implicit_variable_types = true,
-
-					csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-					csharp_enable_inlay_hints_for_types = true,
-					dotnet_enable_inlay_hints_for_indexer_parameters = true,
-					dotnet_enable_inlay_hints_for_literal_parameters = true,
-					dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-					dotnet_enable_inlay_hints_for_other_parameters = true,
-					dotnet_enable_inlay_hints_for_parameters = true,
-					dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-					dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-					dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
 				},
 				["csharp|code_lens"] = {
 					dotnet_enable_references_code_lens = true,
@@ -49,3 +51,17 @@ return {
 		})
 	end,
 }
+
+-- return {
+-- 	"seblyng/roslyn.nvim",
+-- 	ft = "cs",
+-- 	config = function()
+-- 		local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+--
+-- 		require("roslyn").setup({
+-- 			dotnet_cmd = "dotnet",
+-- 			roslyn_version = "4.8.0-3.23475.7",
+-- 		})
+-- 	end,
+-- }
