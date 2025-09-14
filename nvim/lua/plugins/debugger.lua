7f671b62af6127c5f52a5e87380ebbf36f2d3683
martin.grgv <martin.geor04@gmail.com>
2025-09-14 17:02:43 +0300

    nvim debugger fixes

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -9,64 +9,44 @@ return { -- C# Debugger
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
+		local dapwidgets = require("dap.ui.widgets")
 
+		require("easy-dotnet.netcoredbg").register_dap_variables_viewer()
 		dapui.setup()
+
 		dap.adapters.coreclr = {
 			type = "executable",
-			command = "/usr/bin/netcoredbg",
+			command = "/usr/local/netcoredbg",
 			args = { "--interpreter=vscode" },
 		}
 
 		dap.configurations.cs = {
 			{
-				name = "Run .NET Core",
 				type = "coreclr",
+				name = "launch - netcoredbg",
 				request = "launch",
-				cwd = vim.fn.getcwd(),
-				env = {
-					ASPNETCORE_ENVIRONMENT = "Development",
-				},
 				program = function()
-					vim.notify("Started debugging...", vim.log.levels.DEBUG)
-
-					local cwd = vim.fn.getcwd()
-					local projectName = vim.fn.fnamemodify(cwd, ":t")
-					local dllPattern = cwd .. "/bin/Debug/net*/" .. projectName .. ".dll"
-					local projectDlls = vim.fn.glob(dllPattern, false, true)
-
-					vim.notify("cwd: " .. cwd, vim.log.levels.DEBUG)
-					vim.notify("project name: " .. projectName, vim.log.levels.DEBUG)
-
-					if #projectDlls == 0 then
-						vim.notify("❌ Project DLL not found! Did you forget to build?", vim.log.levels.ERROR)
-						return vim.fn.input("Path to DLL: ", cwd .. "/", "file")
-					end
-
-					local dllPath = projectDlls[1]
-					vim.notify("dll path: " .. dllPath, vim.log.levels.DEBUG)
-
-					if vim.fn.filereadable(dllPath) == 1 then
-						return dllPath
-					end
-
-					vim.notify("❌ DLL not readable!", vim.log.levels.ERROR)
-					return vim.fn.input("Path to DLL: ", cwd .. "/", "file")
+					return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
 				end,
-				before = function(config)
-					print("Building...")
-
-					local result = vim.fn.system("dotnet build")
-					if vim.v.shell_error ~= 0 then
-						vim.notify("Build failed:\n" .. result, vim.log.levels.ERROR)
-						print("Build failed")
-						return false
-					end
-
-					print("Build succeeded")
-					return true
-				end,
+				stopOnEntry = false,
+				env = "Development",
 			},
 		}
+
+		dap.listeners.before["launch"].build_project = function()
+			print("Building...")
+
+			local sln = vim.fn.glob("**/*.sln")
+			vim.notify("Building project solution: " .. sln)
+
+			local result = vim.fn.system("dotnet build " .. sln)
+
+			if vim.v.shell_error ~= 0 then
+				vim.notify("Build failed:\n" .. result, vim.log.levels.ERROR)
+			end
+
+			vim.notify("Build success:\n" .. result, vim.log.levels.INFO)
+		end
 
 		vim.keymap.set("n", "<F5>", function()
 			dap.continue()
@@ -74,6 +54,9 @@ return { -- C# Debugger
 		vim.keymap.set("n", "<F6>", function()
 			dap.terminate()
 		end, { desc = "Stop Debugging" })
+		vim.keymap.set("n", "<F7>", function()
+			dap.restart()
+		end, { desc = "Restart Session" })
 		vim.keymap.set("n", "<F10>", function()
 			dap.step_over()
 		end, { desc = "Step Over" })
@@ -92,9 +75,27 @@ return { -- C# Debugger
 		vim.keymap.set("n", "<Leader>du", function()
 			dapui.toggle()
 		end, { desc = "Toggle DAP UI" })
+		vim.keymap.set("n", "<Leader>wh", function()
+			dapwidgets.hover()
+		end, { desc = "Hover widgets" })
+		vim.keymap.set("n", "<Leader>vp", function()
+			dapwidgets.preview()
+		end, { desc = "Preview widgets" })
+		vim.keymap.set("n", "<Leader>wt", function()
+			dapwidgets.centered_float(dapwidgets.frames)
+		end, { desc = "Stack Trace" })
+		vim.keymap.set("n", "<Leader>wr", function()
+			dapwidgets.centered_float(dapwidgets.scopes)
+		end, { desc = "Scopes" })
 		vim.keymap.set("n", "<Leader>dj", function()
-			dap.goto_(vim.fn.getcurpos()[2])
-		end, { desc = "Debugger - Set next statement" })
+			dap.focus_frame()
+		end, { desc = "Focus current frame" })
+		vim.keymap.set("n", "<Leader>dc", function()
+			dap.goto_()
+		end, { desc = "Jump to cursor" })
+		vim.keymap.set("n", "<Leader>dr", function()
+			dap.run_to_cursor()
+		end, { desc = "Run to cursor" })
 
 		-- Auto open/close dap-ui when debugging starts/stops
 		dap.listeners.after.event_initialized["dapui_config"] = function()
